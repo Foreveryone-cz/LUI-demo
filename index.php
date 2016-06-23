8b854562919acc7041f21869c145006b55d35211
Jiří Bělský <jiri.belsky@4every1.cz>
2016-06-23 10:58:37 +0200

    Flexibility removed - didn't fix flexbox in IE9; Will look for different solution for at least partial compatibility

--- index.php
+++ index.php
@@ -29,8 +29,6 @@
 	<script src="/lui/js/jquery/jquery-2.2.0.min.js"></script>
 	<script src="/lui/js/jquery/jquery-ui.js"></script>
 	<script src="/lui/js/jquery/ui/ui.js"></script>
-
-	<script src="/js/flexibility/flexibility.min.js"></script>
 </head>
 <body>
 
